807c3617c2055367843cae528c70ababec5fc682
Harris <bsclmr146620@spu.ac.ke>
2024-06-07 23:54:21 +0300

    added :tickets

---
+++ src/app/@dashboard/@usertickets.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+function UserTickets(props: Props) {
+    const {} = props
+
+    return (
+        <h2>Tickets</h2>
+    )
+}
+
+export default UserTickets
